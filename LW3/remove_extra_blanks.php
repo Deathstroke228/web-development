e1d43391c20b8e68cecb923dc353694899fdd97b
Deathstroke228 <61599965+Deathstroke228@users.noreply.github.com>
2020-10-25 20:16:50 +0300

    Add files via upload

---
+++ LW3/remove_extra_blanks.php
@@ -0,0 +1,20 @@
+<?php
+
+header("Content-Type: text/plain");
+if ($_GET['text'])
+{
+    $name = $_GET['text'];
+    $name = trim($name);
+    $length = strlen($name);
+    for ($i = 0; $i <= $length; $i++)
+    {
+        $name = str_replace("  ", " ", $name);
+    }
+    echo $name;
+}    
+
+
+  
+
+
+
---
+++ LW3/survey_server.php
@@ -0,0 +1,14 @@
+<?php
+
+$firstName = (isset($_GET['first_name'])) ? $_GET['first_name'] : ' ';
+$lastName = (isset($_GET['last_name'])) ? $_GET['last_name'] : ' ';
+$email = (isset($_GET['email'])) ? $_GET['email'] : ' ';
+$age = (isset($_GET['age'])) ? $_GET['age'] : ' ';
+if (!empty($email))
+{
+    $data = [$firstName, $lastName, $email, $age];
+    $data = implode("|", $data);
+    mkdir('data/');
+    $file = "data/" . $email . ".txt";
+    file_put_contents($file, $data);
+}
