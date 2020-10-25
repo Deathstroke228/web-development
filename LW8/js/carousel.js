630dceeb6462a38dbf52fff789507bae7ed6e269
Deathstroke228 <61599965+Deathstroke228@users.noreply.github.com>
2020-10-26 02:19:29 +0300

    Add files via upload

---
+++ LW8/js/carousel.js
@@ -0,0 +1,31 @@
+function carousel() {
+    const leftArrow = document.getElementById('left_arrow');
+    const rightArrow = document.getElementById('right_arrow');
+    const films = document.querySelectorAll('.cell');
+    const listOfFilms = document.querySelector('.carousel');
+    let numberOfFilms = films.length - 1;
+    let i = 0;
+
+    function slideRight(){
+        listOfFilms.appendChild(films[i]);
+        if (i < numberOfFilms) {
+            i++;
+        } else {
+            i = 0;
+        }
+    }
+
+    function slideLeft(){
+        if (i <= 0) {
+            i = numberOfFilms;
+            listOfFilms.insertBefore(films[numberOfFilms], films[numberOfFilms - i]);
+        } else {
+            i--;
+            listOfFilms.insertBefore(films[i], films[i + 1]);
+        }
+    }
+    rightArrow.addEventListener("click", slideRight);
+    leftArrow.addEventListener("click", slideLeft);
+}
+
+window.onload = carousel;
